3884c78222ad2db93935f3c4b816330991c10753
Kashif Iftikhar <kashif@compulife.com.pk>
2011-05-21 18:16:09 +0500

    updated UI designer file and moved code so there is no code loss when re-generating UI code

--- DNSNG_UI.py
+++ DNSNG_UI.py
@@ -2,13 +2,12 @@
 
 # Form implementation generated from reading ui file 'DNS-NG-UI.ui'
 #
-# Created: Sat May 14 18:16:53 2011
+# Created: Sat May 21 18:12:32 2011
 #      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -18,21 +17,19 @@ except AttributeError:
 class Ui_dlgDNS(object):
     def setupUi(self, dlgDNS):
         dlgDNS.setObjectName(_fromUtf8("dlgDNS"))
-        dlgDNS.resize(543, 487)
+        dlgDNS.resize(592, 522)
         self.verticalLayoutWidget = QtGui.QWidget(dlgDNS)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 541, 481))
+        self.verticalLayoutWidget.setGeometry(QtCore.QRect(0, 0, 591, 521))
         self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
         self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetMinAndMaxSize)
+        self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetDefaultConstraint)
         self.verticalLayout.setMargin(0)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
-        
         self.tblRedirects = QtGui.QTableWidget(self.verticalLayoutWidget)
         self.tblRedirects.setMinimumSize(QtCore.QSize(400, 200))
         self.tblRedirects.setObjectName(_fromUtf8("tblRedirects"))
         self.tblRedirects.setColumnCount(6)
         self.tblRedirects.setRowCount(0)
-        self.tblRedirects.setToolTip('Current redirect rules in the DNS database')
         item = QtGui.QTableWidgetItem()
         self.tblRedirects.setHorizontalHeaderItem(0, item)
         item = QtGui.QTableWidgetItem()
@@ -46,34 +43,40 @@ class Ui_dlgDNS(object):
         item = QtGui.QTableWidgetItem()
         self.tblRedirects.setHorizontalHeaderItem(5, item)
         self.verticalLayout.addWidget(self.tblRedirects)
-        
-        self.horizontalLayout_0 = QtGui.QHBoxLayout()
-        self.horizontalLayout_0.setObjectName(_fromUtf8("horizontalLayout_0"))
-        spacerItem_0 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout_0.addItem(spacerItem_0)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.cmdAddRecord = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.cmdAddRecord.setObjectName(_fromUtf8("cmdAddRecord"))
+        self.horizontalLayout_2.addWidget(self.cmdAddRecord)
+        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout_2.addItem(spacerItem)
         self.cmdRefresh = QtGui.QPushButton(self.verticalLayoutWidget)
         self.cmdRefresh.setObjectName(_fromUtf8("cmdRefresh"))
-        self.horizontalLayout_0.addWidget(self.cmdRefresh)
-        self.verticalLayout.addLayout(self.horizontalLayout_0)
-        
+        self.horizontalLayout_2.addWidget(self.cmdRefresh)
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
         self.txtDNSRequests = QtGui.QTextBrowser(self.verticalLayoutWidget)
         self.txtDNSRequests.setEnabled(True)
         self.txtDNSRequests.setMinimumSize(QtCore.QSize(400, 200))
         self.txtDNSRequests.setObjectName(_fromUtf8("txtDNSRequests"))
-        self.txtDNSRequests.setToolTip('DNS Log')
         self.verticalLayout.addWidget(self.txtDNSRequests)
-        
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
+        self.rdo_on = QtGui.QRadioButton(self.verticalLayoutWidget)
+        self.rdo_on.setStyleSheet(_fromUtf8("background-color: rgb(0, 255, 0);"))
+        self.rdo_on.setChecked(True)
+        self.rdo_on.setObjectName(_fromUtf8("rdo_on"))
+        self.horizontalLayout.addWidget(self.rdo_on)
+        self.rdo_off = QtGui.QRadioButton(self.verticalLayoutWidget)
+        self.rdo_off.setStyleSheet(_fromUtf8("background-color: rgb(255, 0, 0);"))
+        self.rdo_off.setObjectName(_fromUtf8("rdo_off"))
+        self.horizontalLayout.addWidget(self.rdo_off)
+        spacerItem1 = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem1)
         self.cmdClose = QtGui.QPushButton(self.verticalLayoutWidget)
         self.cmdClose.setObjectName(_fromUtf8("cmdClose"))
         self.horizontalLayout.addWidget(self.cmdClose)
         self.verticalLayout.addLayout(self.horizontalLayout)
-        
-        self.setLayout(self.verticalLayout)
-        
+
         self.retranslateUi(dlgDNS)
         QtCore.QMetaObject.connectSlotsByName(dlgDNS)
 
@@ -85,31 +88,17 @@ class Ui_dlgDNS(object):
         self.tblRedirects.horizontalHeaderItem(3).setText(QtGui.QApplication.translate("dlgDNS", "Query Domain RE", None, QtGui.QApplication.UnicodeUTF8))
         self.tblRedirects.horizontalHeaderItem(4).setText(QtGui.QApplication.translate("dlgDNS", "Query Type", None, QtGui.QApplication.UnicodeUTF8))
         self.tblRedirects.horizontalHeaderItem(5).setText(QtGui.QApplication.translate("dlgDNS", "Enabled?", None, QtGui.QApplication.UnicodeUTF8))
+        self.cmdAddRecord.setToolTip(QtGui.QApplication.translate("dlgDNS", "Add a record", None, QtGui.QApplication.UnicodeUTF8))
+        self.cmdAddRecord.setText(QtGui.QApplication.translate("dlgDNS", "Add Record", None, QtGui.QApplication.UnicodeUTF8))
+        self.cmdRefresh.setText(QtGui.QApplication.translate("dlgDNS", "Refresh", None, QtGui.QApplication.UnicodeUTF8))
         self.txtDNSRequests.setHtml(QtGui.QApplication.translate("dlgDNS", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"></p></body></html>", None, QtGui.QApplication.UnicodeUTF8))
+        self.rdo_on.setToolTip(QtGui.QApplication.translate("dlgDNS", "Display incoming DNS requests", None, QtGui.QApplication.UnicodeUTF8))
+        self.rdo_on.setText(QtGui.QApplication.translate("dlgDNS", "On", None, QtGui.QApplication.UnicodeUTF8))
+        self.rdo_off.setToolTip(QtGui.QApplication.translate("dlgDNS", "Turn off incoming DNS requests display", None, QtGui.QApplication.UnicodeUTF8))
+        self.rdo_off.setText(QtGui.QApplication.translate("dlgDNS", "Off", None, QtGui.QApplication.UnicodeUTF8))
         self.cmdClose.setText(QtGui.QApplication.translate("dlgDNS", "Close", None, QtGui.QApplication.UnicodeUTF8))
-        self.cmdRefresh.setText(QtGui.QApplication.translate("dlgDNS", "Refresh", None, QtGui.QApplication.UnicodeUTF8))
-        
-        #Signal handler connections
-        self.connect(self.cmdClose, QtCore.SIGNAL("clicked()"), QtGui.qApp, QtCore.SLOT("quit()"))
-        
-    
-    def closeEvent(self, event):
-        reply = QtGui.QMessageBox.question(self, "Really Quit?", "Are you sure you want to quit?",
-                                           QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.Yes)
-        
-        if reply == QtGui.QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
-    
-    
-    
 
-
-
-    
-    
--- DNS_UI.py
+++ DNS_UI.py
@@ -1,4 +1,5 @@
 from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import Qt
 from DNSNG_UI import Ui_dlgDNS
 from db_model import Redirect, ClientHost, DNSRequest
 from db_code import get_db_session
@@ -10,12 +11,23 @@ class DnsUI(QtGui.QDialog, Ui_dlgDNS):
     def __init__(self):
         QtGui.QDialog.__init__(self)
         self.setupUi(self)
-        self.retranslateUi(self)
+        self.setLayout(self.verticalLayout)
+        
+        #Signal handler connections
+        self.connect(self.cmdClose, QtCore.SIGNAL("clicked()"), QtGui.qApp, QtCore.SLOT("quit()"))
         self.connect(self.cmdRefresh, QtCore.SIGNAL("clicked()"), self.reloadRedirects)
         
         self.objDB = get_db_session()
         self.reloadRedirects()
     
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, "Really Quit?", "Are you sure you want to quit?",
+                                           QtGui.QMessageBox.Yes | QtGui.QMessageBox.No, QtGui.QMessageBox.Yes)
+        
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
     
     def reloadRedirects(self):
         redirects = self.objDB.query(Redirect)
@@ -23,7 +35,10 @@ class DnsUI(QtGui.QDialog, Ui_dlgDNS):
         self.tblRedirects.setRowCount(redirects.count())
         
         for r_num, R in enumerate(redirects):
-            self.tblRedirects.setItem(r_num, 0, QtGui.QTableWidgetItem(str(R.rule_id)))
+            item = QtGui.QTableWidgetItem(str(R.rule_id))
+            item.setFlags(Qt.ItemIsEnabled|Qt.ItemIsSelectable)
+            
+            self.tblRedirects.setItem(r_num, 0, item)
             self.tblRedirects.setItem(r_num, 1, QtGui.QTableWidgetItem(R.redirect_ip))
             self.tblRedirects.setItem(r_num, 2, QtGui.QTableWidgetItem(R.client_host_specs))
             self.tblRedirects.setItem(r_num, 3, QtGui.QTableWidgetItem(R.query_domain))
